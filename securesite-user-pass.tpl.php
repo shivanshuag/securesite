56f2af635594f354eb973ec158bc2f1f963edd38
Darren Oh <darren@oh.name>
2008-12-22 20:14:16 +0000

    Made password reset error messages show on separate page. Fixed guest password storage for digest authentication. Added index of tests. Finished functional tests.

--- securesite-user-pass.tpl.php
+++ securesite-user-pass.tpl.php
@@ -7,7 +7,6 @@
  */
 ?>
 <h1><?php print t('Password reset') ?></h1>
-<?php print $messages ?>
 <p><?php print $title ?></p>
 <?php print drupal_render($form['name']); ?>
 <?php print drupal_render($form['submit']); ?>
