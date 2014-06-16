dbdcca4ca8605d469ab8658c26d87d45b8b89e82
Shivanshu Agrawal <shivanshuag@gmail.com>
2014-06-16 23:29:29 +0530

    started porting tests

---
+++ src/Tests/ForcedAuth/SecureSiteForceDisabledTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @file
+ * Contains Drupal\securesite\Tests\ForcedAuth\SecureSiteForceDisabledTest
+ */
+
+use Drupal\simpletest\WebTestBase;
+
+/**
+ * Functional test for page request without forced authentication.
+ */
+class SecureSiteForceDisabledTest extends WebTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = array('securesite');
+
+  /**
+   * Implements getInfo().
+   */
+  public static function getInfo() {
+    return array(
+      'name' => t('Forced authentication: Disabled'),
+      'description' => t('Test page request without forced authentication.'),
+      'group' => t('Secure Site'),
+    );
+  }
+
+  /**
+   * Implements setUp().
+   */
+  function setUp() {
+    //todo find a replacement
+    parent::setUp();
+  }
+
+  /**
+   * Request home page without forced authentication.
+   */
+  function testSecureSiteForceDisabled() {
+    $this->drupalHead(NULL);
+    $this->assertResponse(200, t('Requesting home page.'));
+  }
+}
