b3e20b63061439e769679efd23ebc8cc6c9f1b54
Shivanshu Agrawal <shivanshuag@gmail.com>
2014-07-04 21:20:16 +0530

    minor refactor

--- src/EventSubscriber/SecuresiteSubscriber.php
+++ src/EventSubscriber/SecuresiteSubscriber.php
@@ -11,10 +11,7 @@ use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Drupal\securesite\SecuresiteManagerInterface;
-use Drupal\Core\Authentication\AuthenticationManager;
-use Drupal\Core\Session\AnonymousUserSession;
 
-//use \Drupal\basic_auth\Authentication\Provider\BasicAuth;
 /**
  * Subscribes to the kernel request event to check whether authentication is required
  */
@@ -47,28 +44,22 @@ class SecuresiteSubscriber implements EventSubscriberInterface {
    *   The event to process.
    */
   public function onKernelRequest(GetResponseEvent $event) {
-    //var_dump($event->getRequest()->attributes->get('_authentication_provider'));
     $account = \Drupal::currentUser();
     $this->manager->setRequest($event->getRequest());
 
     // Did the user send credentials that we accept?
     $type = $this->manager->getMechanism();
-    var_dump($account->id());
     if ($type !== FALSE && (isset($_SESSION['securesite_repeat']) ? !$_SESSION['securesite_repeat'] : TRUE)) {
-      //authentication is done by SecuresiteAuth authentication provider. Nothing here
       $this->manager->boot($type);
     }
     // If credentials are missing and user is not logged in, request new credentials.
-    //todo check if $account->id() == 0 works
     elseif ($account->id() == 0 && !isset($_SESSION['securesite_guest'])) {
-      var_dump('show dialog');
       if (isset($_SESSION['securesite_repeat'])) {
         unset($_SESSION['securesite_repeat']);
       }
       if ($this->manager->forcedAuth()) {
         $types = \Drupal::config('securesite.settings')->get('securesite_type');
         sort($types, SORT_NUMERIC);
-        var_dump('forced');
         $this->manager->showDialog(array_pop($types));
       }
     }
